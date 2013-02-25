7f6e66a2dc526d5fcff864983d9a7f7d23f4022d
Brad Anderson <brad@sankatygroup.com>
2013-02-25 08:35:27 -0500

    merging Ted's latest pull request

--- src/main/java/com/mapr/demo/twitter/Twokenizer.java
+++ src/main/java/com/mapr/demo/twitter/Twokenizer.java
@@ -58,6 +58,7 @@ public class Twokenizer implements Serializable {
     // not in the string 'abcword'
     private static Pattern Contractions = Pattern.compile("(?i)^(\\w+)(n't|'ve|'ll|'d|'re|'s|'m)$");
     private static Pattern Whitespace = Pattern.compile("\\s+");
+    private static Pattern StopWords = Pattern.compile("^(rt|i|me|my|myself|we|us|our|ours|ourselves|you|your|yours|yourself|yourselves|he|him|his|himself|she|her|hers|herself|it|its|itself|they|them|their|theirs|themselves|what|which|who|whom|whose|this|that|these|those|am|is|are|was|were|be|been|being|have|has|had|having|do|does|did|doing|will|would|should|can|could|ought|i'm|you're|he's|she's|it's|we're|they're|i've|you've|we've|they've|i'd|you'd|he'd|she'd|we'd|they'd|i'll|you'll|he'll|she'll|we'll|they'll|isn't|aren't|wasn't|weren't|hasn't|haven't|hadn't|doesn't|don't|didn't|won't|wouldn't|shan't|shouldn't|can't|cannot|couldn't|mustn't|let's|that's|who's|what's|here's|there's|when's|where's|why's|how's|a|an|the|and|but|if|or|because|as|until|while|of|at|by|for|with|about|against|between|into|through|during|before|after|above|below|to|from|up|upon|down|in|out|on|off|over|under|again|further|then|once|here|there|when|where|why|how|all|any|both|each|few|more|most|other|some|such|no|nor|not|only|own|same|so|than|too|very|say|says|said|shall)$");
 
     private static String punctChars = "['“\\\".?!,:;]";
     private static String punctSeq = punctChars + "+";
@@ -135,6 +136,11 @@ public class Twokenizer implements Serializable {
    public String squeezeWhitespace(String input) {
        Matcher whitespaceMatcher = Whitespace.matcher(input);
        return whitespaceMatcher.replaceAll(" ").trim();
+   }
+
+   public boolean stopWord(String input) {
+       Matcher matcher = StopWords.matcher(input);
+       return matcher.matches();
    }
 
    // For special patterns
@@ -240,6 +246,7 @@ public class Twokenizer implements Serializable {
                if (!token.isEmpty()
                        && !token.matches(punctChars)
                        && !token.matches(entity)
+                       && !stopWord(token)
                        ) {
                    finalTokens.add(token);
                }
@@ -249,7 +256,8 @@ public class Twokenizer implements Serializable {
        return finalTokens;
    }
 
-   // the twokenize method which filters out white spaces before using simpleTokenize()
+   // the twokenize method which filters out white spaces and stop words before
+   // using simpleTokenize()
    public List<String> twokenize(String text) {
        return simpleTokenize(squeezeWhitespace(text));
    }
