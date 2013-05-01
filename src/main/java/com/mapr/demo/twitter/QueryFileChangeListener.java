0ef82eaf716f95a69d781c39653115d768fe3c95
Brad Anderson <brad@sankatygroup.com>
2013-04-30 23:40:28 -0400

    catch ServiceException

--- src/main/java/com/mapr/demo/twitter/QueryFileChangeListener.java
+++ src/main/java/com/mapr/demo/twitter/QueryFileChangeListener.java
@@ -3,10 +3,16 @@ package com.mapr.demo.twitter;
 import java.io.File;
 import java.io.IOException;
 
+import com.google.protobuf.ServiceException;
+
 public class QueryFileChangeListener implements FileChangeListener {
 
     public void fileChanged(File file) throws IOException {
-        TweetLogger.changeQuery();
+        try {
+            TweetLogger.changeQuery();
+        } catch (ServiceException e) {
+            e.printStackTrace();
+        }
     }
 
 }
