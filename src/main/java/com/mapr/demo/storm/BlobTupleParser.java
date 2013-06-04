b8400616f906628325b6e31fb6603c9fe672862e
Brad Anderson <brad@sankatygroup.com>
2013-06-03 22:27:44 -0400

    make BlobTupleParser serializable, try two fields

--- src/main/java/com/mapr/demo/storm/BlobTupleParser.java
+++ src/main/java/com/mapr/demo/storm/BlobTupleParser.java
@@ -1,5 +1,6 @@
 package com.mapr.demo.storm;
 
+import java.io.Serializable;
 import java.util.List;
 
 import backtype.storm.tuple.Fields;
@@ -9,18 +10,18 @@ import com.google.common.collect.Lists;
 import com.google.protobuf.ByteString;
 import com.mapr.ProtoSpout.TupleParser;
 
-public class BlobTupleParser extends TupleParser {
+public class BlobTupleParser extends TupleParser implements Serializable {
 
-    Splitter onSpace = Splitter.on(" ");
+    private static final long serialVersionUID = 3297543209369011198L;
 
     @Override
     public List<Object> parse(ByteString buffer) {
-        return Lists.<Object> newArrayList(onSpace.split(buffer.toStringUtf8()));
+        return Lists.<Object> newArrayList(Splitter.on(" ").split(buffer.toStringUtf8()));
     }
 
     @Override
     public Fields getOutputFields() {
-        return new Fields("content");
+        return new Fields("topic", "content");
     }
 
 }
